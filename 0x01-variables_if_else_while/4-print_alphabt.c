a28c95719eae8d292bd26392a54bd40db70f5e6f
gemebasic <gemuderiba@gmail.com>
2022-07-04 06:59:14 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main- display char except q
+ * Return: always return 0
+*/
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= z; x++)
+	{
+		if (x != 'e' && x != 'q')
+			putchar(x);
+		putchar('\n');
+	}
+	return (0);
+}
