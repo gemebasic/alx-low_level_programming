3fa67c4e3cc53db84b65d8d645488d77433ebf74
gemebasic <gemuderiba@gmail.com>
2022-07-04 07:26:32 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main- display char
+ * Return: always return 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
