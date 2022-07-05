1783db2b2e1477aa60594c8da2e2aad401907c4a
gemebasic <gemuderiba@gmail.com>
2022-07-05 03:04:11 -0700

    hot fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * void print_alphabet_x10 -> print lower case alphabet
+ * print_alphabet_x10-> print lower case alphabet
  * ten times
  */
 void print_alphabet_x10(void)
@@ -12,8 +12,7 @@ void print_alphabet_x10(void)
 	for (i = 0; i < 10; i++)
 	{
 		for (x = 'a'; x <= 'z'; x++)
-		{
 			_putchar(x);
 			_putchar('\n');
-		}
 	}
+}
