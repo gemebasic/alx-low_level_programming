26dc1fee7fe44d2a69d07952e366002969536d1a
gemebasic <gemuderiba@gmail.com>
2022-07-05 05:57:02 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add-> to add two number
+ * @a: function argument
+ * @b: function argument
+ * Return: return result
+ */
+int add(int a, int b)
+{
+	int x;
+
+	x = a + b;
+	return (x);
+}
