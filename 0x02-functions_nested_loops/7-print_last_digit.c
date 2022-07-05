4209a4135dfb596502f63783a9a47599ecaa63f6
gemebasic <gemuderiba@gmail.com>
2022-07-05 04:43:47 -0700

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit-> to print lasat digit of number
+ * @x: argument for the function
+ * Return: return last digit
+ */
+int print_last_digit(int x)
+{
+	x = x % 10;
+	return (x);
+}
