090fa8d4513e10c13d98a18bc257bb94bd7482b9
gemebasic <gemuderiba@gmail.com>
2022-07-05 04:14:27 -0700

    task

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign-> display positive or negative
+ * @n: argument for intiger
+ * Return: negative or posative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
