315dfcb334eca41ee3a317bb328b3b4169b39c8a
gemebasic <gemuderiba@gmail.com>
2022-07-07 00:56:42 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers-> code to preint number
+ * Return: always return 0
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		_putchar(i);
+	_putchar('\n');
+}
