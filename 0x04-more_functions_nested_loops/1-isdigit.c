b4f148d9ba2922c58727a31ababa278acf618b16
gemebasic <gemuderiba@gmail.com>
2022-07-07 00:33:45 -0700

    task1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit-> is tocheck weather it is digit or not
+ * @c: argument fro function
+ * Retrurn: succes
+ * Return: succes
+ *
+ */
+int _isdigit(int c)
+{
+	if (c <= 0 && c >= 9)
+		return (1);
+	return (0);
+}
