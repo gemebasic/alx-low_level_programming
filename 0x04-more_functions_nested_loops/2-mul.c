685d64556282fedadcc8a9ff1155b1bde5fb83bb
gemebasic <gemuderiba@gmail.com>
2022-07-07 00:46:29 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul-> multiplay numbers
+ * @a: argument of the function
+ * @b: argumenmt of the function
+ * Return: succes
+ *
+ */
+int mul(int a, int b)
+{
+	int res;
+
+	res = a * b;
+	return (res);
+}
