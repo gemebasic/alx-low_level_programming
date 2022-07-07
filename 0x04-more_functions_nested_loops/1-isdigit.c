301071731012fe338b1fce0ffcbb1b70c2f4f8f8
gemebasic <gemuderiba@gmail.com>
2022-07-07 05:03:45 -0700

    hot fix

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * _isdigit-> is tocheck weather it is digit or not
+ * _isdigit-> is tocheck weather it is 
+ * digit or not
  * @c: argument fro function
  * Return: succes
  *
  */
 int _isdigit(int c)
 {
-	return (c >= 0 && c <= 9);
+	return (c >= '0' && c <= '9');
 }
