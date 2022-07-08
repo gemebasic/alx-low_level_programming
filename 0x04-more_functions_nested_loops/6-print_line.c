35b65ff19b6e49ee902a46969d59d1a594f39120
gemebasic <gemuderiba@gmail.com>
2022-07-08 05:23:17 -0700

    hot ifx

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,9 +11,9 @@ void print_line(int n)
 
 	if (n <= 0)
 		_putchar('\n');
-		for (b = 0; b <= n; b++)
-		{
-			_putchar('_');
-		}
-		_putchar('\n');
+	for (b = 0; b <= n; b++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
 }
