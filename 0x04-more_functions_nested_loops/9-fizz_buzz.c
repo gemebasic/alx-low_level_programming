06e03c2fa32e2e5e23d36f5be95d6011e097072c
gemebasic <gemuderiba@gmail.com>
2022-07-08 01:13:41 -0700

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main- main function
+ * Returns: awyas returns 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; 1 <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf("Buzz");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 != 0 && i % 5 != 0)
+		{
+			printf("\d", i);
+		}
+	}
+	printf("\n");
+	return (0);
+}
