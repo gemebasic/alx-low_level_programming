ba507b1c2e5a9c0789d3a42f0c3a32ffc23cf7e7
gemebasic <gemuderiba@gmail.com>
2022-07-11 04:30:54 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * puts2-> pritn every other chararcter
+ * @str: argument
+ */
+void puts2(char *str)
+{
+	int i, ln;
+
+	ln = strlen(str);
+	for (i = 0; i < ln; i += 2)
+		_putchar(str[i]);
+	_putchar('\n');
+}
