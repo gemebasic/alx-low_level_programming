8d6304f6a97f7860115aae78c33f3ae4f9ad1d75
gemebasic <gemuderiba@gmail.com>
2022-07-11 05:47:09 -0700

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_array-> to print array
+ * @a: function argument
+ * @n: function argumnet
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		printf(a[i]);
+}
