ee13668c3e1c0346a139cc5d1e570242314aca5c
gemebasic <gemuderiba@gmail.com>
2022-07-11 01:39:43 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen-> return length of string
+ * @s: function agument
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s != '\0')
+		i++;
+	return (i);
+}
