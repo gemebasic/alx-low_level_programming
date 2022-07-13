07447ad2c70447ab3753c76b91f25c4e7a27ff80
gemebasic <gemuderiba@gmail.com>
2022-07-13 04:03:44 -0700

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strcmp-> compare string
+ * @s1: argument1
+ * @s2: argument 2
+ * Return: return value
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int x;
+
+	x = strcmp(s1, s2);
+	return (x);
+}
