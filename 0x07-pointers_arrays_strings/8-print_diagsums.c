20b185d076ac7153bbe269b30c0e25e06262e3bd
gemebasic <gemuderiba@gmail.com>
2022-07-18 06:38:04 -0700

    task 7

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums-> function
+ * @a: first ARGUMENT
+ * @size: second argument
+ */
+void print_diagsums(int *a, int size)
+{
+	int b, sum1 = 0, sum2 = 0;
+
+	for (b = 0; b < size; b++)
+	{
+		sum1 += a[(size + 1) + b];
+		sum2 += a[(size - 1) + (b + 1)];
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
