797061d50b12b67791d75c40f7e9409e2f513de5
gemebasic <gemuderiba@gmail.com>
2022-07-25 03:52:22 -0700

    head file

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,5 +1,7 @@
-#ifndef
-#define
+#ifndef MAIN_H
+#define MAIN_H
+#include <stddef.h>
+#include <stdlib.h>
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
